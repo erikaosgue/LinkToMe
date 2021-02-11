eb8fdb7df843dc30f5b21847ba6c8f3f1bfcd5fd
erikaosgue <1476@holbertonschool.com>
2021-02-10 23:55:07 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  get "users", to: "user#list_all_users"
-  post "users", to: "user#create_user"
-  put "users", to: "user#update_user"
-  delete "users", to: "user#delete_user"
+  get "users", to: "users#index"
+  post "users", to: "users#create"
+  put "users/:id", to: "users#update"
+  delete "users/:id", to: "users#destroy"
+  get "users/:id", to: "users#show"
+
 
   root to: "main#index"
 
