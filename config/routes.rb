0e06cc8063d7103287f5f618a901e73b73554f72
erikaosgue <1476@holbertonschool.com>
2021-02-11 14:55:57 -0500

    Update routes.rb file for new links routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
 
+  # Controller for user
   get "users", to: "users#index"
   post "users", to: "users#create"
   put "users/:id", to: "users#update"
   delete "users/:id", to: "users#destroy"
   get "users/:id", to: "users#show"
+  get "users/:id/links", to: "users#links"
+
+
+  #routes and controller for links
+  get "links", to: "links#index"
+  post "links", to: "links#create"
+  put "links/:id", to: "links#update"
+  delete "links/:id", to: "links#destroy"
+  get "links/:id", to: "links#show"
+  get "links/:id/user", to: "links#user"
 
 
   root to: "main#index"
