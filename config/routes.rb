cd33fb8b546af8c7b3ba43d107cdd5259f275846
erikaosgue <1476@holbertonschool.com>
2021-02-15 20:56:09 -0500

    Change routes for resources model instead of the manual created ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,11 @@
 Rails.application.routes.draw do
 
   # Controller for user
+  resources :users
   get "profiles", to: "users#index"
   get "myprofile", to: "users#myprofile"
-  get "users", to: "users#new"
-  post "users", to: "users#create"
-  put "users/:id", to: "users#update"
-  delete "users/:id", to: "users#destroy"
-  get "users/:id", to: "users#show"
-  get "users/:id/links", to: "users#links"
-
-
-  #routes and controller for links
-  get "links", to: "links#index"
-  post "links", to: "links#create"
-  put "links/:id", to: "links#update"
-  delete "links/:id", to: "links#destroy"
-  get "links/:id", to: "links#show"
-  get "links/:id/user", to: "links#user"
-
+  
+  resources :links
 
   root to: "main#index"
 
