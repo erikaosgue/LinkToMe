9658c76adb6346d7127fc9436700e6b45af22372
erikaosgue <1476@holbertonschool.com>
2021-02-17 23:18:37 -0500

    Add avatar to users controller

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ApplicationRecord
     belongs_to :user
-    validates :app_name, presence: true
-    validates :url, presence: true
+    validates :app_name, presence: {message: "App name shouldn't not be empty"}
+    validates :url, presence: {message: "Url shouldn't be empty"}
 end
