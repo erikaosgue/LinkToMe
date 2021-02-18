f3659b4fd72d008254f205c1d0453e6bfd2d25fa
erikaosgue <1476@holbertonschool.com>
2021-02-17 23:18:44 -0500

    Add avatar to users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
     has_many :links, dependent: :destroy
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+    has_one_attached :avatar
 end
